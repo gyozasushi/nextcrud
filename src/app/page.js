1126220b1b666c6d42f4d94f67924789a38bee81
gyozasushi <denisabizar54@gmail.com>
2024-08-23 17:22:06 +0700

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { Container, Heading, Table, Th, Thead, Tbody, Tr, Td } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { axiosInstance } from "@/app/lib/axios";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    try {
+      const productsResponse = await axiosInstance.get("/products");
+      setProducts(productsResponse.data);
+      console.log(productsResponse.data); // Pastikan data yang diterima dari API
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderProducts = () => {
+    return products.map((product) => (
+      <Tr key={product.id}>
+        <Td>{product.id}</Td>
+        <Td>{product.name}</Td>
+        <Td>{product.price}</Td>
+        <Td>{product.description}</Td>
+        <Td>{product.image}</Td>
+      </Tr>
+    ));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <Container>
+        <Heading>Hello World</Heading>
+        
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Id</Th>
+              <Th>Name</Th>
+              <Th>Price</Th>
+              <Th>Description</Th>
+              <Th>Images</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {renderProducts()}
+          </Tbody>
+        </Table>
+      </Container>
     </main>
   );
 }
